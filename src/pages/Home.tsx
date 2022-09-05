31723b2e586f1c3795badbc4e91d2ee89c0d35ef
hc precog <chao921125@sina.com>
2022-09-06 01:38:35 +0800

    up pkg

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 // @ts-ignore
 import { Shake } from "reshake";
+
 // https://elrumordelaluz.github.io/reshake/
 // https://animxyz.com/docs#react-installation
 // https://animista.net/play/basic
 export default function Index() {
+
 	return (
 		<>
 			<Shake h={5} v={10} r={2} dur={300} int={10} max={100} fixed={true} fixedStop={false} freez={false}>
