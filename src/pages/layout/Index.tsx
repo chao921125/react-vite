acd68ea8e400b6293e846eb17cac9bad2d0e3ef8
cc <hcprecog@gmail.com>
2022-09-05 22:15:19 +0800

    add animation

--- src/pages/layout/Index.tsx
+++ src/pages/layout/Index.tsx
@@ -1,8 +1,32 @@
 import { Outlet } from "react-router-dom";
+import { Layout } from "antd";
+
 export default function Index() {
+	const { Header, Footer, Sider, Content } = Layout;
+
+	const isAdmin = false;
+
 	return (
-		<div>
-			<Outlet />
-		</div>
+		<>
+			{isAdmin && (
+				<Layout>
+					<Sider>Sider</Sider>
+					<Layout>
+						<Header>Header</Header>
+						<Content>
+							<Outlet />
+						</Content>
+						<Footer>Footer</Footer>
+					</Layout>
+				</Layout>
+			)}
+			{!isAdmin && (
+				<Layout>
+					<Content>
+						<Outlet />
+					</Content>
+				</Layout>
+			)}
+		</>
 	);
 }
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,5 @@
 // 动态路由解决方案 https://juejin.cn/post/7132393527501127687
+// 动态路由解决方案 https://zhuanlan.zhihu.com/p/518339176
 import { useRoutes } from "react-router-dom";
 import { Suspense, lazy } from "react";
 
