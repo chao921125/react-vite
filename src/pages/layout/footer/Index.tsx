f5e445fdb5a77858ebdb01a76f7a338f5cfe141f
huangchao <77830871+hc-advokate@users.noreply.github.com>
2023-05-22 17:58:55 +0800

    增加管理

---
+++ src/pages/layout/footer/Index.tsx
@@ -0,0 +1,12 @@
+import "./index.scss";
+
+export default function Footer() {
+	return (
+		<>
+			<div className="re-w-fill re-flex-row-center">
+				<a>京ICP备10036305号-1</a>
+				<a>京公网安备11000002000001号</a>
+			</div>
+		</>
+	);
+}
