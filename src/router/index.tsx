d174c4fc4b95c071f7e550fbcf97d905627441f9
huangchao <77830871+hc-advokate@users.noreply.github.com>
2023-06-06 18:02:33 +0800

    Update index.tsx

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -31,7 +31,7 @@ const generateRouter = (routers: IMenu[]) => {
 		return item;
 	});
 };
-
+console.log(JSON.stringify(routes));
 export const Routers = () => useRoutes(generateRouter(routes));
 
 //根据路径获取路由
@@ -56,5 +56,32 @@ export const lazyLoad = (componentPathName: string) => {
 	const Module = lazy(() => import(`pages/${componentPathName}`));
 	return <Module />;
 };
+
+const viewsModules: any = import.meta.glob("../pages/**/**.{vue,tsx}");
+const dynamicViewsModules: Record<string, Function> = Object.assign({}, { ...viewsModules });
+console.log(routeToComponent([{ name: "demo", component: "demo/Screen" }]));
+function routeToComponent(routes: any[]) {
+	if (!routes) return [];
+	return routes.map((item: any) => {
+		if (item.component) item.component = componentImport(dynamicViewsModules, item.component as string);
+		item.children && routeToComponent(item.children);
+		return item;
+	});
+}
+
+function componentImport(viewsModules: Record<string, Function>, component: string) {
+	const keys = Object.keys(viewsModules);
+	const matchKeys = keys.filter((key) => {
+		const k = key.replace(/..\/pages|../, "");
+		return k.startsWith(`${component}`) || k.startsWith(`/${component}`);
+	});
+	if (matchKeys?.length === 1) {
+		const matchKey = matchKeys[0];
+		return viewsModules[matchKey];
+	}
+	if (matchKeys?.length > 1) {
+		return false;
+	}
+}
 
 export default { Routers, checkRouterAuth };
