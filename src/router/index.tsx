72982d8f60ffef2f52d147abb3ccae18b0b250ce
huangchao <77830871+hc-advokate@users.noreply.github.com>
2023-06-07 17:09:06 +0800

    feat: 动态路由研究中······

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -72,29 +72,42 @@ export const checkRouterAuth = (path: String) => {
 let Routers = () => useRoutes(generateRouter(routes));
 
 // 动态处理路由
+const pagesModules: any = import.meta.glob("../pages/**/**.{vue,tsx}");
+const dynamicPagesModules: Record<string, Function> = Object.assign({}, { ...pagesModules });
 export const setRouter = () => {
-	rootRouter = [...rootRouter, ...routeToComponent(RouterData.menus)];
+	const dy = routeToComponent(RouterData.menus);
+	console.log(dy);
+	rootRouter = [...rootRouter, ...dy];
 	baseRouters[0].children = [...rootRouter];
-	console.log(baseRouters);
 	routes = [...errorRouters, ...baseRouters];
 	Routers = () => useRoutes(generateRouter(routes));
 };
 
-export function routeToComponent(routes: any[]) {
+function routeToComponent(routes: any[]) {
 	if (!routes) return [];
 	return routes.map((item: any) => {
 		if (item.component) {
 			item.path = `/${item.path}`;
-			item.component = componentImport(item.component as string);
+			item.component = componentImport(dynamicPagesModules, item.component as string);
 		}
 		item.children && routeToComponent(item.children);
 		return item;
 	});
 }
 
-function componentImport(component: string) {
-	const path = `pages/${component}`;
-	return lazy(() => import(path));
+function componentImport(viewsModules: Record<string, Function>, component: string) {
+	const keys = Object.keys(viewsModules);
+	const matchKeys = keys.filter((key) => {
+		const k = key.replace(/..\/pages|../, "");
+		return k.startsWith(`${component}`) || k.startsWith(`/${component}`);
+	});
+	if (matchKeys?.length === 1) {
+		const matchKey = matchKeys[0];
+		return viewsModules[matchKey];
+	}
+	if (matchKeys?.length > 1) {
+		return false;
+	}
 }
 
 export default Routers;
